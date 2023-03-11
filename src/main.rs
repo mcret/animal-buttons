ad0db63615a0ea68775a4c49b97f1f193fef80b2
retsehc <retsehc@gmail.com>
2023-03-11 13:08:34 -0600

    added debouncing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use rppal::gpio::{Gpio, InputPin, Trigger};
 
-use std::{fs, io};
+use std::{fs};
 use std::fs::{DirEntry, File};
 use std::io::{BufReader};
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::time::{Duration, Instant};
 
 use log::{info, LevelFilter};
 use simple_logger::SimpleLogger;
@@ -18,12 +19,15 @@ fn main() -> ! {
     info!("Hello, pets!");
 
     //set up buttons
-    let (_stream, stream_handle) = OutputStream::try_default().expect("Unable to get default output stream");
+    let (_stream, stream_handle) = OutputStream::try_default()
+        .expect("Unable to get default output stream");
     let aud_path = Path::new("audio");
     let gpio = Gpio::new().expect("Unable to create new gpio");
-    for dir in 1..10
+    let mut pins: Vec<InputPin> = Vec::new();
+    for dir in 4..=4
     {
-        let sink = Sink::try_new(&stream_handle).expect(&*format!("Unable to sink for pin {}", dir));
+        let sink = Sink::try_new(&stream_handle)
+            .expect(&*format!("Unable to sink for pin {}", dir));
         let path_buf = aud_path.join(dir.to_string());
         let file = fs::read_dir(path_buf.clone())
             .expect(&*format!("Unable to read directory {:?}", path_buf))
@@ -31,9 +35,15 @@ fn main() -> ! {
             .expect(&*format!("Unable to find file in {:?}", path_buf))
             .expect("I don't know what goes here");
 
-        let mut pin = gpio.get(dir).expect(&*format!("unable to get pin {}", dir)).into_input_pullup();
-        pin.set_async_interrupt(Trigger::FallingEdge, move |_| foo(&sink, &file, dir))
+        info!("configuring in {}", dir);
+        info!("with file {:?}", file.file_name());
+
+        let mut pin = gpio.get(dir)
+            .expect(&*format!("unable to get pin {}", dir)).into_input_pullup();
+        let mut debouncer = Debouncer::new(sink, file.path(), dir);
+        pin.set_async_interrupt(Trigger::RisingEdge, move |_| debouncer.foo())
             .expect(&*format!("Unable to set interrupt on pin {}", dir));
+        pins.push(pin);
     }
 
     loop {
@@ -41,11 +51,42 @@ fn main() -> ! {
     }
 }
 
-fn foo(sink: &Sink, file: &DirEntry, dir: u8)
+struct Debouncer
 {
-    let reader = BufReader::new(File::open(file.path()).expect(&*format!("Unable to open file {:?}", dir)));
-    let source = Decoder::new(reader).expect(&*format!("Unable to create encoder for {:?}", dir));
+    last_trigger: Instant,
+    min_duration: Duration,
+    sink: Sink,
+    file: PathBuf,
+    dir: u8,
+}
 
-    info!("Callback for button {}", dir);
-    (*sink).append(source);
+impl Debouncer
+{
+    fn new(sink: Sink, file: PathBuf, dir: u8) -> Debouncer
+    {
+        Debouncer
+        {
+            last_trigger: Instant::now(),
+            min_duration: Duration::from_secs(1),
+            sink,
+            file,
+            dir,
+        }
+    }
+
+    fn foo(&mut self)
+    {
+        if self.last_trigger.elapsed() < self.min_duration
+        {
+            return;
+        }
+        self.last_trigger = Instant::now();
+        let reader = BufReader::new(File::open(&self.file)
+            .expect(&*format!("Unable to open file {:?}", self.dir)));
+        let source = Decoder::new(reader)
+            .expect(&*format!("Unable to create encoder for {:?}", self.dir));
+
+        info!("Callback for button {}", dir);
+        self.sink.append(source);
+    }
 }
